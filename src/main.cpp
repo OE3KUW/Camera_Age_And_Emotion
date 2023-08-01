94519f82b6b2756127e6e8f0a9aaff184253e328
OE3KUW <wolfgang.kuran@htlstp.ac.at>
2023-08-01 07:05:44 +0200

    Initial

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+/***************************************************************************************
+//
+//                c a m e r a   a g e   a n d   e m o t i o n       
+//
+//                                                                      қuran jul 2023
+**************************************************************************************/
+/*********
+  Rui Santos
+  Complete instructions at https://RandomNerdTutorials.com/esp32-cam-projects-ebook/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*********/
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
